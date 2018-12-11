51783bfb7e5d2d4ab60c573bfe8c068967143018
Alex <agonopol@gmail.com>
2018-12-11 14:11:20 +0100

    [ag] getting further, now need to merge/weigh new points accordingly

--- src/pycondense/condense.py
+++ src/pycondense/condense.py
@@ -22,17 +22,21 @@ class Condensator:
         if 'sigma' in kwargs:
             self.sigma = kwargs['sigma']
         else:
-            self.sigma = 2.2204e-16
+            self.sigma = np.spacing(1)
         if 'epsilon' in kwargs:
             self.epsilon = kwargs['epsilon']
         else:
             knn = NearestNeighbors().fit(self.data)
             distances, _ = knn.kneighbors(self.data)
-            self.epsilon = np.percentile(distances[:, 4], .95)
+            self.epsilon = np.percentile(distances[:, 4], 95, interpolation='midpoint')
         if 'affinity' in kwargs:
             self.affinityfn = kwargs['affinity']
         else:
             self.affinityfn = affinity
+        if 'i' in kwargs:
+            self.i = kwargs['i']
+        else:
+            self.i = 1
 
     def contract(self):
         condesator = self
@@ -42,21 +46,23 @@ class Condensator:
 
     def next(self):
         diffused = self.diffuse()
-        distances = squareform(pdist(diffused, metric='sqeuclidean'))
-        if np.any(distances < self.sigma - np.eye(len(distances), len(distances))):
-            [merge, condesed] = self.merge(diffused, distances)
-            return merge, Condensator(condesed, sigma=self.sigma + math.e, n=self.n, epsilon=self.epsilon,
-                                       affinity=self.affinityfn)
+        [merged, condensed] = self.merge( diffused )
+        if merged:
+            return merged, Condensator( condensed, sigma=self.sigma + math.e, n=self.n, epsilon=self.epsilon,
+                                       affinity=self.affinityfn, i=self.i + 1)
         else:
-            return [], Condensator(diffused, sigma=self.sigma, n=self.n, epsilon=self.epsilon * 1.005,
-                                   affinity=self.affinityfn)
+            epsilon = self.epsilon * 1.05 if self.i % 200 == 0 else self.epsilon
+            return [], Condensator( diffused, sigma=self.sigma, n=self.n, epsilon=epsilon,
+                                   affinity=self.affinityfn, i=1)
 
     def diffuse(self):
         affinity = self.affinity()
-        data = ((affinity / affinity.sum(1)) ** 2) @ self.data
+        norm = (affinity / affinity.sum(1)).transpose()
+        data = np.linalg.matrix_power(norm, 2) @ self.data
         return data
 
-    def merge(self, diffused, distances):
+    def merge(self, diffused ):
+        distances = squareform(pdist(diffused, metric='sqeuclidean'))
         filtered = (distances < self.sigma ** 2) - np.eye(len(distances))
         n, labels = connected_components(filtered)
         if n == len(filtered):
@@ -78,22 +84,10 @@ class Condensator:
 
 
 if __name__ == '__main__':
-    import pandas
+    import scipy.io as sio
     import os
-    # Predefine choice
-    choice = [279, 899, 727,  11, 195, 701, 413, 389, 371, 858, 300, 668,  94,
-        36, 487, 279, 219, 976, 670, 873, 206, 786,  71, 667,  19, 134,
-       524, 513, 417, 682, 860, 482, 481,   0, 148,  37, 469, 521, 768,
-       602, 996, 841, 650,  22, 675, 390, 603, 361,  25, 305, 723, 582,
-       862, 917, 538, 967, 908, 633, 501, 854, 807, 453, 333, 572, 536,
-         2, 771, 139, 363, 620, 250, 639, 722, 904, 882, 446, 875, 765,
-       509, 838, 199, 356, 126, 531, 752, 881, 143, 818,  72, 805, 338,
-       642, 166,  88, 200, 318,  75, 258, 732, 769]
-
-
-    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "data", "circles.csv")) as circles:
-        data = pandas.read_csv(circles)
-        condesator = Condensator(data.values[choice])
-        for i, assigment in enumerate(condesator.contract()):
-            print( i, assigment )
+    sample = sio.loadmat(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "data", "sample.mat"))
+    condesator = Condensator(sample['pc'])
+    for i, assigment in enumerate(condesator.contract()):
+        print( i, assigment )
 
--- tests/test_condense.py
+++ tests/test_condense.py
@@ -1,25 +1,47 @@
 import pytest
 from pycondense.condense import Condensator
+import numpy as np
 
 __author__ = "Alex"
 __copyright__ = "Alex"
 __license__ = "mit"
 
+
 @pytest.fixture
 def circles():
-    import pandas
+    import scipy.io as sio
     import os
-    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "data", "circles.csv")) as circles:
-        return pandas.read_csv(circles)
+    mat = sio.loadmat(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "data", "circles.mat"))
+    return mat['data']
 
-def test_affinity( circles ):
-    condesator = Condensator( circles )
-    assert condesator.epsilon == pytest.approx( 0.012591470540472, .001 )
+
+def test_affinity(circles):
+    condesator = Condensator(circles)
+    assert condesator.epsilon == pytest.approx(0.012591470540472, .001)
     affinity = condesator.affinity()
     assert affinity is not None
-    assert affinity[0, 1]  == pytest.approx( 0.778798853737574, .001 )
+    assert affinity[0, 1] == pytest.approx(0.778798853737574, .001)
 
-def test_contraction( circles ):
-    condesator = Condensator( circles )
+
+def test_contraction(circles):
+    condesator = Condensator(circles)
     contraction = condesator.contract()
     assert contraction is not None
+
+def test_affinity_matrix():
+    import scipy.io as sio
+    import os
+    sample = sio.loadmat(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "data", "sample.mat"))
+    condesator = Condensator(sample['pc'])
+    assert condesator.epsilon == pytest.approx(0.22928802334138387, .001)
+    diffused = sio.loadmat(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "data", "diffused.mat"))
+    affinity = condesator.affinity()
+    assert np.isclose(affinity, diffused['kern'], .01).all()
+
+def test_diffuse( ):
+    import scipy.io as sio
+    import os
+    sample = sio.loadmat(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "data", "sample.mat"))
+    condesator = Condensator(sample['pc'])
+    diffused = sio.loadmat(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "data", "diffused.mat"))
+    assert np.isclose(condesator.diffuse( ), diffused['pc'], .01).all( )
